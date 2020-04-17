e8ca87115ee2d1e355277311fd1ec2ff98922638
Jumts <julianamattos201313@gmail.com>
2020-04-17 20:02:20 -0300

    done

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,38 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = { income: 0, outcome: 0, total: 0 };
+
+    this.transactions.filter((transaction: Transaction): null => {
+      if (transaction.type === 'income') {
+        balance.income += transaction.value;
+        balance.total += transaction.value;
+      } else {
+        balance.outcome += transaction.value;
+        balance.total -= transaction.value;
+      }
+      return null;
+    });
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    if (type === 'outcome') {
+      const { total } = this.getBalance();
+
+      if (value > total) {
+        throw Error('Value in balance insufficient');
+      }
+    }
+
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
